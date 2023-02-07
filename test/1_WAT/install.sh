0693a7d76972ded8954063b8344033de2e95eb73
eggduzao <eggduzao@gmail.com>
2023-02-07 11:55:09 +0200

    separating tests

---
+++ test/1_WAT/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pip install --user /home/egg/Projects/SingleCellRnaSeq/
+#pip install --user /Users/egg/Projects/SingleCellRnaSeq/
+
+
+
---
+++ test/1_WAT/test_pipeline.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Arguments
+input_matrix="--input_matrix=/home/egg/Projects/SingleCellRnaSeq/test/1_WAT/data/GSM4717152_CD45N-L1.csv"
+temporary_location="--temporary_location=/home/egg/Projects/SingleCellRnaSeq/test/1_WAT/temp/"
+output_location="--output_location=/home/egg/Projects/SingleCellRnaSeq/test/1_WAT/output/"
+
+#input_matrix="--input_matrix=/Users/egg/Projects/SingleCellRnaSeq/test/1_WAT/data/GSM4717152_CD45N-L1.csv"
+#temporary_location="--temporary_location=/Users/egg/Projects/SingleCellRnaSeq/test/1_WAT/temp/"
+#output_location="--output_location=/Users/egg/Projects/SingleCellRnaSeq/test/1_WAT/output/"
+
+# Options
+organism="--organism=hg38"
+input_type="--input-type=csv"
+cores="--cores=4"
+output_type="--output-type=csv"
+seed="--seed=123"
+
+# Hidden Options
+io_input_file_is_reversed="--io-input-file-is-reversed"
+
+
+# Execution
+
+scaw $organism $input_type $cores $output_type $seed $io_input_file_is_reversed $input_matrix $temporary_location $output_location 
+
+
